a16331ab6f62bc0c42318b23c39f17064771f1f0
Duckierpine3108 <jalilkamran63@gmail.com>
2026-08-24 16:02:39 +0500

    fix prisma generate build script

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true, // Bypasses TypeScript errors during Vercel build
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Bypasses ESLint errors during Vercel build
+    ignoreBuildErrors: true,
   },
 };
 
